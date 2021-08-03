d5fa3bdc1ac63a7765662cd96552d0ee82ea9fa1
jangwoni <rejwe79@gmail.com>
2021-08-03 11:29:06 +0900

    Update argparse1.py

--- argparse/argparse1.py
+++ argparse/argparse1.py
@@ -1,11 +1,15 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='사용법 테스트')
+# 파서 만들기
+parser = argparse.ArgumentParser(description = '테스트')
 
-parser.add_argument('--target', required=True , help='어느 것을 요구하냐')
-parser.add_argument('--env', required=False, default='dev', help='실행환경은 뭐냐')
+# 인자 추가하기
+parser.add_argument('--a', required=True , action='append', help='입력1')
+parser.add_argument('--b', required=False, default='dev', help='입력2')
+parser.add_argument('--zoo1', type=str, nargs=2, help='입력3')
 
+# 인자 파싱하기
 args = parser.parse_args()
 
-print(args.target)
-print(args.env)
+print(args.a)
+print(args.b)
